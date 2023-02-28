462125b16a137ae4895b91d10a6e37ddf308ea14
ajaysharma12799 <ajaysharma12799@gmail.com>
2023-02-28 22:23:44 +0530

    added Redux-Persist to Persist State

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
-import { Store } from "./redux/Store";
+import { Store, persistor } from "./redux/Store";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={Store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
